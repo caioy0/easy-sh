97c85ab34394d7cc68f067aeff0dd6cf9ffeb538
caio <caio.yugo11@gmail.com>
2025-03-12 15:43:50 -0300

    feat: omp zsh swap

--- installmint.sh
+++ installmint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get upgrade
-
 sudo apt install ani-cli zsh neovim code
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
