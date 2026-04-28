774975aa1665936ca833bcb0e72903e281219913
caio <caio.yugo@proton.me>
2026-04-28 18:49:09 -0300

    chore(hyprland): packages

--- arch-base/hyprland.sh
+++ arch-base/hyprland.sh
@@ -4,4 +4,5 @@ yay -S waypaper waybar matugen-bin sddm-git wireplumber \
         pipewire qt5-wayland qt6-wayland fnott \
         xdg-desktop-portal-hyprland-git \
         quickshell-git caelestia-shell qtmultimedia \
-        wofi
+        wofi xdg-desktop-portal pavucontrol gvfs \
+        xorg-xwayland
